42a026155d832fe3c996f374ab572e8518f7c873
Konstantinos Melios <constantinemelios@gmail.com>
2020-11-13 10:22:52 +0200

    Try fixing smaller mobile screen

--- src/styles/TvSetStyle.js
+++ src/styles/TvSetStyle.js
@@ -16,7 +16,7 @@ const TvSetStyle = styled.div`
   filter: drop-shadow(0 3px 6px rgba(0,0,0,0.56));
   transition: transform 0.2s ease;
   position: relative;
-  width: 360px;
+  width: 300px;
   animation: ${attentionAnimation} 4s infinite, ${entranceAnimation} 1s 1;
   :hover,
   :active {
